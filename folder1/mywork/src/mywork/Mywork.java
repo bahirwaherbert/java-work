ebbec1ef306ed074bc3c4ac14074fd36d3757e86
bahirwaherbert <2300723935@mubs.ac.ug>
2024-10-04 02:32:21 -0700

    Add files via upload

---
+++ folder1/mywork/src/mywork/Mywork.java
@@ -0,0 +1,17 @@
+package mywork;
+
+import java.util.Scanner;
+
+public class Mywork {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=0;
+//Do-while loop
+do {
+	System.out.println(x);
+	x++;
+}while(x<=5);
+
+	}
+
+}
